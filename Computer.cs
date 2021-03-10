a018b1c1f6a0baefee0d8f645e590da2f571f212
Jackanthon <73250214+Jackanthon@users.noreply.github.com>
2021-03-10 08:11:13 -0600

    Add files via upload

---
+++ Computer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rockpaper
+{
+    class Computer
+    {
+        ///Ranomizes a number 1-3 for Rock Paper Scissors
+        ///Assign 1-3 to RPS
+        ///if 1 and 3
+                
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Rockpaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Rock Paper Scissors!");
+
+
+            do
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Random r = new Random();
+                    int computerC = r.Next(3);
+                    Console.WriteLine("Choose either rock, paper, or scissors");
+                    string userC = Console.ReadLine();
+                    if (computerC == 0)
+                    {
+                        Console.WriteLine("The computer chooses Rock");
+                        if (userC == "rock")
+                        {
+                            Console.WriteLine("It's a tie.");
+                        }
+                        else if (userC == "paper")
+                        {
+                            Console.WriteLine("Congrats, you've won!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lost!");
+                        }
+                    }
+                    else if (computerC == 1)
+                    {
+                        Console.WriteLine("The computer chooses Paper");
+                        if (userC == "rock")
+                        {
+                            Console.WriteLine("You lost!");
+                        }
+                        else if (userC == "paper")
+                        {
+                            Console.WriteLine("It's a tie");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Congrats, you've won!!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("The computer chooses Scissors");
+                        if (userC == "rock")
+                        {
+                            Console.WriteLine("Congrat's, you've won!!");
+                        }
+                        else if (userC == "paper")
+                        {
+                            Console.WriteLine("You lost!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("It's a tie");
+                        }
+                    }
+                }
+            }
+            while (Console.ReadLine() == "y");
+
+        }
+    }
+}
